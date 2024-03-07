3cf1796683df4ce346337c5df62d2a2dfa2037d3
AlexsanderDiego <alexdiegoat@gmail.com>
2024-03-07 01:06:02 -0300

    Atualizações na página de login

--- src/pages/TelaLogin.jsx
+++ src/pages/TelaLogin.jsx
@@ -1,23 +1,14 @@
 import { Button, Col, Flex, Form, Input, Row } from "antd";
 import FormItem from "antd/es/form/FormItem/index.js";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import axios from "axios";
-
-import Cadastro from "./TelaCadastro";
-import TelaRedefinirSenha from "./TelaRedefinirSenha.jsx";
-import TelaUserAdmin from "./TelaUserAdmin.jsx";
 
 //import do css
 import "../css/TelaLogin.css";
 
 function AuthLogin() {
-  const [telaAdmin, setTelaAdmin] = useState(null);
-  const [telaCadastro, setTelaCadastro] = useState(null);
-  const [telaRedefinirSenha, setTelaRedefinirSenha] = useState(null);
-  const [userId, setUserId] = useState(null);  
-
+  
   const navigate = useNavigate();
 
   async function login(dados) {
@@ -25,33 +16,11 @@ function AuthLogin() {
       const resposta = await axios.post("https://fs01backend.onrender.com/auth/login", dados);
 
       console.log("Resposta do login:", resposta.data);
-      // setTelaAdmin(true);
       navigate("/admin/" + resposta.data.id);
       alert("Login Autorizado");
-      setUserId(resposta.data.id)
     } catch (error) {
       alert("Erro ao fazer login. Por favor, tente novamente.");
     }
-  }
-
-  // if (telaAdmin) {
-  //   return <TelaUserAdmin idLogado={userId}/>;
-  // }
-  // if (telaCadastro) {
-  //   return <Cadastro />;
-  // }
-  // if (telaRedefinirSenha) {
-  //   return <TelaRedefinirSenha />;
-  // }
-
-  function RedefinirSenha() {
-    // setTelaRedefinirSenha(true);
-    navigate("/redefinirsenha");
-  }
-
-  function TelaCadastro() {
-    // setTelaCadastro(true);
-    navigate("/cadastro");
   }
 
   return (
@@ -100,16 +69,20 @@ function AuthLogin() {
             </Col>
             <Col>
               <Form.Item>
-                <Button type="primary" onClick={TelaCadastro}>
+                <Link to="/cadastro">
+                <Button type="primary">
                   Cadastrar novo usuário
                 </Button>
+                </Link>
               </Form.Item>
             </Col>
             <Col>
               <FormItem>
-                <Button type="danger" onClick={RedefinirSenha}>
+              <Link to="/redefinirsenha">
+                <Button type="danger">
                   <span className="btnEsqueciSenha">Esqueceu sua senha?</span>
                 </Button>
+                </Link>
               </FormItem>
             </Col>
           </Row>
